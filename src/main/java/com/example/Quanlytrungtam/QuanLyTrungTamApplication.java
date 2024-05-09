b61e6c4fd77644aa6083c5f8237151781cb3b852
XuanQui <ngqui133@gmail.com>
2024-05-09 12:17:26 +1000

    delele file

---
+++ src/main/java/com/example/Quanlytrungtam/QuanLyTrungTamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quanlytrungtam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyTrungTamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyTrungTamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Quanlytrungtam/QuanLyTrungTamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quanlytrungtam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyTrungTamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
